77455df55acda6c0d9049aa40440041d6f6a14f9
Sanjot Boyal <sboyal2@uwo.ca>
2018-01-30 03:12:06 -0500

    Finished Assignment 1
    Assignment 1 Finished Build

---
+++ Assets/__Scripts/BoxTilt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class boxTilt : MonoBehaviour {
+
+
+	void OnMouseDown(){
+		transform.Rotate(new Vector3 (40, 0, 0) * Time.deltaTime);
+	}
+}
---
+++ Assets/__Scripts/ResetObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetObject : MonoBehaviour {
+	private Vector3 initialPosition;
+	private Quaternion initialRotation;
+	// Use this for initialization
+	void Start () {
+		initialPosition = gameObject.transform.position;
+		initialRotation = gameObject.transform.rotation;
+	}
+	
+	void OnMouseDown(){
+		gameObject.transform.position = initialPosition;
+		gameObject.transform.rotation = initialRotation;
+	}
+}
